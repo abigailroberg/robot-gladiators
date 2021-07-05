8e40bb96d256c17d0abbdd95d951292f8fbc4e55
Abby Roberg <abbyroberg@gmail.com>
2021-07-05 17:51:44 -0500

    added while loop to fight multiple robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,8 +19,8 @@ var enemyAttack = 12;
 
 // fight function
 var fight = function(enemyName) {
-  // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
+  // Repeat and execute as long as the enemy-robot is alive
+  while(enemyHealth > 0) {
 
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
@@ -71,10 +71,13 @@ var fight = function(enemyName) {
   } else {
     window.alert("You need to pick a valid option. Try again!");
   }
+}
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
 }
 
 // run fight function to start game
